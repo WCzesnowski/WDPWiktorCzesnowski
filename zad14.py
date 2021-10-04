b7c23edcf378cfeb1491c9210d0b8314bb051bc5
WCzesnowski <91889551+WCzesnowski@users.noreply.github.com>
2021-10-04 14:43:50 +0200

    Add files via upload

---
+++ zad14.py
@@ -0,0 +1,6 @@
+import math
+a = 3
+b = 7
+alfa = 45
+p = 1/2*3*7*math.sin(alfa)
+print (p)
---
+++ zad15.py
@@ -0,0 +1,7 @@
+import math
+x_a = 0
+y_a = 0
+x_b = 3
+y_b = 4
+d= math.sqrt((x_b - x_a)*(x_b - x_a) + (y_b - y_a)*(y_b - y_a))
+print (d)
