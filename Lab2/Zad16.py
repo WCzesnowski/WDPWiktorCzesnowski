58f571af66c79e405e2b2539b7b28a48be7b448b
WCzesnowski <91889551+WCzesnowski@users.noreply.github.com>
2021-10-12 11:57:10 +0200

    Add files via upload

---
+++ Lab2/Zad16.py
@@ -0,0 +1,18 @@
+import math
+x = 1567
+t = math.floor(x/1000)
+s = math.floor((x - t*1000)/100)
+d = math.floor((x - t*1000 - s*100)/10)
+j = math.floor((x - t*1000 - s*100 - d*10))
+if x>99 and x<9999:
+    print(t)
+    print(s)
+    print(d)
+    print(j)
+
+
+
+
+
+
+
