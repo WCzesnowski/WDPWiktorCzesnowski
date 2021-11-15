27f5662317838da734efa165c92594850cdcd637
Wiktor Czesnowski <w.czesnowski.627@studms.ug.edu.pl>
2021-11-15 06:45:14 +0100

    ZAD6_Final

---
+++ LAB05/ZAD6.2.py
@@ -0,0 +1,24 @@
+from random import randint
+from random import seed
+
+#wymiary tablicy
+n = 7
+m = 7
+#sposób 1
+lista = []
+a = [0] * n
+for i in range(n):
+    a[i] = [0] * m
+    for j in range(m):
+        a[i][j]= randint(0,9)
+while n >=0:
+    n -= 1
+    for x in range(n):
+        lista.append(a[x][x+(m-n)])
+
+for z in range(m):
+    print(a[z])
+print()
+print()
+print(lista)
+print(sum(lista))
