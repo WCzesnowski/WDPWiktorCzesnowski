6100c1f1a14a9ddefdae64f006118eef84b80c0b
Wiktor Czesnowski <w.czesnowski.627@studms.ug.edu.pl>
2021-11-21 14:25:57 +0100

    LAB06_zad7

--- Lab6/zad7.py
+++ Lab6/zad7.py
@@ -1,10 +1,14 @@
+import math
 liczba = input()
 liczba = int(liczba)
+
 binary = []
 while liczba !=0:
     if liczba % 2 == 0:
         binary.append(0)
-        liczba = liczba - (liczba/2)
-    if liczba % 2 != 0:
+        liczba = (liczba/2)
+    else:
         binary.append(1)
-        liczba = liczba - (liczba/2) - 1
+        liczba = math.floor(liczba/2)
+binary.reverse()
+print(binary)
