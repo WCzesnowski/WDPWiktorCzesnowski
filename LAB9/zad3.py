8c4a664c2d54e2184b851b52b443b369740e3889
wczesnowski <wiktor.adam.czesnowski@gmail.com>
2021-12-06 14:28:11 +0100

    zad3LAB9

--- LAB9/zad3.py
+++ LAB9/zad3.py
@@ -10,4 +10,4 @@ for i in range(len(lista)):
         for j in range(len(lista) - 1):
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
-                print(lista)
+print(lista)
