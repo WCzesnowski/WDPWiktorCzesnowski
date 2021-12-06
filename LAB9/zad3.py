e2a22ea1b80bf096d07ef9445c47f718f70b2e3e
wczesnowski <wiktor.adam.czesnowski@gmail.com>
2021-12-06 14:26:26 +0100

    zad3LAB9

---
+++ LAB9/zad3.py
@@ -0,0 +1,13 @@
+lista = []
+i = 0
+while i < 10:
+    button=input()
+    x = str(button)
+    lista.append(x)
+    i += 1
+
+for i in range(len(lista)):
+        for j in range(len(lista) - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                print(lista)
